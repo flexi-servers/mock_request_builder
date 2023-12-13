53f6a15fdb69eeede9fe498a925d040041fd16a4
Max KvR <max.kvr@web.de>
2023-12-13 10:22:40 +0100

    Update Version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 
 setup(name='mock_request_builder',
-      version='1.1',
+      version='1.2',
       description='A generator for simple SQLAlchemy models endpoints in FastAPI',
       long_description=long_description,
       long_description_content_type="text/markdown",
